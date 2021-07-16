c054df40dbb77f5c845d5bbafddd8c6e11e08a2f
Amimul <1805056@ugrad.cse.buet.ac.bd>
2021-07-16 20:14:04 +0600

    First Commit

---
+++ src/test/java/com/rahi/springreactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahi.springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
